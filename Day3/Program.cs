0a690448db76603ee248c2e80e1e2bb72725c8b1
david.ferretti <david.ferretti@underdogfantasy.com>
2024-12-05 13:08:16 -0500

    day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,8 +2,26 @@
 
 var input = File.ReadAllText("input.txt");
 var sum = 0;
+var enabled = true;
 foreach (Match match in MyRegex.Regex.Matches(input))
 {
+    var func = match.Groups["func"].Value;
+
+    if (func == "do")
+    {
+        enabled = true;
+        continue;
+    }
+
+    if (func == "don't")
+    {
+        enabled = false;
+        continue;
+    }
+
+    if (!enabled)
+        continue;
+
     var a = int.Parse(match.Groups[1].Value);
     var b = int.Parse(match.Groups[2].Value);
     Console.WriteLine($"mul({a},{b}) = {a * b}");
@@ -13,6 +31,6 @@ Console.WriteLine($"Sum: {sum}");
 
 static partial class MyRegex
 {
-    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    [GeneratedRegex(@"(?<func>mul)\((\d{1,3}),(\d{1,3})\)|(?<func>do)\(\)|(?<func>don't)\(\)")]
     public static partial Regex Regex { get; }
 }
