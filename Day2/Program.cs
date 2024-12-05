5110af61f8107556311c3dde8e5291c0c9786f5f
david.ferretti <david.ferretti@underdogfantasy.com>
2024-12-05 12:33:29 -0500

    day 2 part 1

---
+++ Day2/Program.cs
@@ -0,0 +1,23 @@
+﻿var safeCount = 0;
+
+foreach (var report in File.ReadLines("input.txt"))
+{
+    var levels = report.Split(' ').Select(int.Parse).ToList();
+    var isIncreasing = levels[0] <= levels[1]; // don't need to handle == since that will be unsafe anyway
+    var diffMultiplier = isIncreasing ? 1 : -1;
+
+    var safe = true;
+    for (var i = 1; i < levels.Count; i++)
+    {
+        if (diffMultiplier * (levels[i] - levels[i - 1]) is 1 or 2 or 3)
+            continue;
+
+        safe = false;
+        break;
+    }
+
+    if (safe)
+        safeCount++;
+}
+
+Console.WriteLine($"Safe: {safeCount}");
