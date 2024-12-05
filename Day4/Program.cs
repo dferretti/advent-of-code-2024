1ecbd331a4324017fee96912ee54151c2175e097
david.ferretti <david.ferretti@underdogfantasy.com>
2024-12-05 13:59:49 -0500

    day 4 part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -10,89 +10,128 @@
 using System.Buffers;
 using System.Text;
 
-var original = File.ReadAllLines("input.txt")
+Part1();
+Part2();
+
+static void Part1()
+{
+    var original = File.ReadAllLines("input.txt")
     .Select(line => line.ToCharArray())
     .ToArray();
 
-var rows = original.Length;
-var cols = original[0].Length;
+    var rows = original.Length;
+    var cols = original[0].Length;
 
-var capacityEstimate = (rows + 1) * (cols + 1) * 4;
-var haystackBuilder = new StringBuilder(capacityEstimate);
+    var capacityEstimate = (rows + 1) * (cols + 1) * 4;
+    var haystackBuilder = new StringBuilder(capacityEstimate);
 
-// copy rows to builder
-foreach (var line in original)
-{
-    haystackBuilder.Append(line);
-    haystackBuilder.Append('|');
-}
-
-// copy columns to builder
-for (int c = 0; c < cols; c++)
-{
-    for (int r = 0; r < rows; r++)
-        haystackBuilder.Append(original[r][c]);
-    haystackBuilder.Append('|');
-}
-
-// copy down-right diagonals to builder
-// evision the table is extended to the left, and we start at -cols and take each diagonal line from the top
-/*
- * original table:
- * A B C
- * A B C
- * A B C
- * 
- * envisioned table:
- * . . . A B C
- * . . . A B C
- * . . . A B C
- * 
- * lets us grab lines like: 
- * . .\.\A B C
- * . . .\A\B C
- * . . . A\B\C
- */
-for (int startC = -cols; startC < cols; startC++)
-{
-    // TODO: could be optimized to skip lines that are too short
-    for (int r = 0; r < rows; r++)
+    // copy rows to builder
+    foreach (var line in original)
     {
-        int c = startC + r;
-        if (c < 0 || c >= cols)
-            continue;
-        haystackBuilder.Append(original[r][c]);
+        haystackBuilder.Append(line);
+        haystackBuilder.Append('|');
     }
-    haystackBuilder.Append('|');
-}
 
-// copy up-right diagonals to builder
-for (int startc = -cols; startc < cols; startc++)
-{
-    for (int r = rows - 1; r >= 0; r--)
+    // copy columns to builder
+    for (int c = 0; c < cols; c++)
     {
-        int c = startc + (rows - 1 - r);
-        if (c < 0 || c >= cols)
-            continue;
-        haystackBuilder.Append(original[r][c]);
+        for (int r = 0; r < rows; r++)
+            haystackBuilder.Append(original[r][c]);
+        haystackBuilder.Append('|');
     }
-    haystackBuilder.Append('|');
+
+    // copy down-right diagonals to builder
+    // evision the table is extended to the left, and we start at -cols and take each diagonal line from the top
+    /*
+     * original table:
+     * A B C
+     * A B C
+     * A B C
+     * 
+     * envisioned table:
+     * . . . A B C
+     * . . . A B C
+     * . . . A B C
+     * 
+     * lets us grab lines like: 
+     * . .\.\A B C
+     * . . .\A\B C
+     * . . . A\B\C
+     */
+    for (int startC = -cols; startC < cols; startC++)
+    {
+        // TODO: could be optimized to skip lines that are too short
+        for (int r = 0; r < rows; r++)
+        {
+            int c = startC + r;
+            if (c < 0 || c >= cols)
+                continue;
+            haystackBuilder.Append(original[r][c]);
+        }
+        haystackBuilder.Append('|');
+    }
+
+    // copy up-right diagonals to builder
+    for (int startc = -cols; startc < cols; startc++)
+    {
+        for (int r = rows - 1; r >= 0; r--)
+        {
+            int c = startc + (rows - 1 - r);
+            if (c < 0 || c >= cols)
+                continue;
+            haystackBuilder.Append(original[r][c]);
+        }
+        haystackBuilder.Append('|');
+    }
+
+    var haystack = haystackBuilder.ToString().AsSpan();
+    var needle = SearchValues.Create(["XMAS", "SAMX"], StringComparison.Ordinal);
+    var count = Count(haystack, needle);
+    Console.WriteLine($"Part 1 Count: {count}");
+
+    // from https://devblogs.microsoft.com/dotnet/performance-improvements-in-net-9/#strings-arrays-spans
+    static int Count(ReadOnlySpan<char> haystack, SearchValues<string> needle)
+    {
+        var count = 0;
+        var index = 0;
+        while ((index = haystack.IndexOfAny(needle)) >= 0)
+        {
+            count++;
+            haystack = haystack.Slice(index + 1);
+        }
+        return count;
+    }
 }
 
-var haystack = haystackBuilder.ToString().AsSpan();
-var needle = SearchValues.Create(["XMAS", "SAMX"], StringComparison.Ordinal);
-var count = Count(haystack, needle);
-Console.WriteLine($"Count: {count}");
-
-// from https://devblogs.microsoft.com/dotnet/performance-improvements-in-net-9/#strings-arrays-spans
-static int Count(ReadOnlySpan<char> haystack, SearchValues<string> needle)
+static void Part2()
 {
+    var original = File.ReadAllLines("input.txt")
+        .Select(line => line.ToCharArray())
+        .ToArray();
+
+    var rows = original.Length;
+    var cols = original[0].Length;
+
     var count = 0;
-    var index = 0;
-    while ((index = haystack.IndexOfAny(needle)) >= 0)
+    for (var c = 1; c < cols - 1; c++)
     {
-        count++;
-        haystack = haystack.Slice(index + 1);
+        for (var r = 1; r < rows - 1; r++)
+        {
+            if (original[r][c] != 'A')
+                continue;
+
+            var w = original[r - 1][c - 1];
+            var x = original[r - 1][c + 1];
+            var y = original[r + 1][c - 1];
+            var z = original[r + 1][c + 1];
+            var test = $"{w}{x}{y}{z}";
+            count += test switch
+            {
+                "MSMS" or "MMSS" or "SMSM" or "SSMM" => 1,
+                _ => 0,
+            };
+        }
     }
-    return count;
+
+    Console.WriteLine($"Part 2 Count: {count}");
 }
