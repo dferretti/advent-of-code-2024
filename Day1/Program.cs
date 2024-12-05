e4f6d9bb0c2094f9b28cde38fd2ef3b499b0be92
david.ferretti <david.ferretti@underdogfantasy.com>
2024-12-05 12:05:39 -0500

    adding day 1 part 1

---
+++ Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var (listA, listB) = File.ReadAllLines("input.txt")
+            .Aggregate(
+                (new List<int>(), new List<int>()),
+                (acc, line) =>
+                {
+                    var items = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    acc.Item1.Add(int.Parse(items[0]));
+                    acc.Item2.Add(int.Parse(items[1]));
+                    return acc;
+                });
+
+        listA.Sort();
+        listB.Sort();
+
+        var totalDistance = 0;
+        for (var i = 0; i < listA.Count; i++)
+        {
+            totalDistance += Math.Abs(listA[i] - listB[i]);
+        }
+
+        Console.WriteLine($"Total distance: {totalDistance}");
+    }
+}
