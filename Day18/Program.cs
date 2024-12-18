98db7a1ac6c21bbdc3bae840396550ccefe030ae
david.ferretti <david.ferretti@underdogfantasy.com>
2024-12-18 06:37:41 -0500

    day 18 part 2

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -11,40 +11,81 @@ for (var y = 0; y < grid.Length; y++)
 foreach (var coord in bytes.Take(steps))
     grid[coord.Y][coord.X] = true;
 
-var pq = new PriorityQueue<Coord, int>();
-var source = start;
-var dist = new Dictionary<Coord, int> { [source] = 0 };
-var prev = new Dictionary<Coord, Coord>();
-pq.Enqueue(source, 0);
-for (var y = 0; y < grid.Length; y++)
+var result = GetShortestPath(grid, start, exit);
+if (result is null)
 {
-    for (var x = 0; x < grid[y].Length; x++)
+    Console.WriteLine("No path found");
+    return;
+}
+Console.WriteLine($"Part 1 distance: {result.Value.Distance}");
+
+var path = FlattenPath(result.Value.Prev, start, exit);
+
+foreach (var coord in bytes.Skip(steps))
+{
+    grid[coord.Y][coord.X] = true;
+    if (!path.Remove(coord)) continue; // coord was not on path, so continue
+
+    // if coord was on our shortest path, recompute
+    result = GetShortestPath(grid, start, exit);
+    if (result is null)
     {
-        var coord = new Coord(x, y);
-        if (coord == source) continue;
-        dist[coord] = int.MaxValue;
-        pq.Enqueue(coord, int.MaxValue);
+        Console.WriteLine($"Part 2: {coord}");
+        return;
     }
+
+    path = FlattenPath(result.Value.Prev, start, exit);
 }
 
-while (pq.TryDequeue(out var node, out var priority))
+Console.WriteLine($"Still have a path: {path.Count}");
+
+static HashSet<Coord> FlattenPath(Dictionary<Coord, Coord> prev, Coord start, Coord exit)
 {
-    if (node == exit) break;
-    foreach (var neighbor in GetNeighbors(grid, node))
+    var path = new HashSet<Coord>();
+    for (var at = exit; at != start; at = prev[at])
+        path.Add(at);
+    return path;
+}
+
+static (int Distance, Dictionary<Coord, Coord> Prev)? GetShortestPath(bool[][] grid, Coord start, Coord exit)
+{
+    var pq = new PriorityQueue<Coord, int>();
+    var source = start;
+    var dist = new Dictionary<Coord, int> { [source] = 0 };
+    var prev = new Dictionary<Coord, Coord>();
+    pq.Enqueue(source, 0);
+    for (var y = 0; y < grid.Length; y++)
     {
-        var alt = dist[node] + 1;
-        if (alt < dist[neighbor])
+        for (var x = 0; x < grid[y].Length; x++)
         {
-            dist[neighbor] = alt;
-            prev[neighbor] = node;
-            pq.Remove(neighbor, out _, out _);
-            pq.Enqueue(neighbor, alt);
+            var coord = new Coord(x, y);
+            if (coord == source) continue;
+            // dist[coord] = int.MaxValue;
+            pq.Enqueue(coord, int.MaxValue);
         }
     }
-}
 
-var shortestDistance = dist[exit];
-Console.WriteLine($"Part 1 distance: {shortestDistance}");
+    while (pq.TryDequeue(out var node, out var priority))
+    {
+        if (node == exit) break;
+        foreach (var neighbor in GetNeighbors(grid, node))
+        {
+            if (!dist.TryGetValue(node, out var d)) continue;
+            var alt = d + 1;
+            if (alt < dist.GetValueOrDefault(neighbor, int.MaxValue))
+            {
+                dist[neighbor] = alt;
+                prev[neighbor] = node;
+                pq.Remove(neighbor, out _, out _);
+                pq.Enqueue(neighbor, alt);
+            }
+        }
+    }
+
+    return dist.TryGetValue(exit, out var shortestDistance)
+        ? (shortestDistance, prev)
+        : null;
+}
 
 static IEnumerable<Coord> GetNeighbors(bool[][] grid, Coord coord)
 {
